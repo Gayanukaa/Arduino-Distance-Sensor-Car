b92bf455bde0b6dfcbde75325c744155376e9686
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-02-03 20:48:31 +0530

    Added variables

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,21 @@
+#define IN1 8
+#define IN2 9
+#define IN3 10
+#define IN4 11
+
+long distance = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT); 
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  return (pulseIn(echoPin, HIGH)*0.01723);
+}
 
 void setup() {
   // put your setup code here, to run once:
